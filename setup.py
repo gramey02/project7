19b6a8e2906696c9aaa46b6717f0c9720661f2f2
Grace Ramey <Grace.Ramey@ucsf.edu>
2022-03-11 01:43:54 -0800

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name= 'nn',
+    version= '0.0.1',
+    author= 'Grace Ramey',
+    author_email= 'Grace.Ramey@ucsf.edu',
+    packages= find_packages(),
+    description= 'Neural network class and data preprocessor',
+	install_requires= ['pytest', 'typing', 'numpy', 'scikit-learn', 'sklearn']
+)
